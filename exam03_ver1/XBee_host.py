5c1d6cc55a9ef5af4167251da1c77d85071f8031
AlvinChuang <alvin870203@gapp.nthu.edu.tw>
2020-06-03 17:03:58 +0800

    complete problem3

--- exam03_ver1/XBee_host.py
+++ exam03_ver1/XBee_host.py
@@ -3,6 +3,39 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 
+import paho.mqtt.client as paho
+import time
+mqttc = paho.Client()
+
+# Settings for connection
+host = "localhost"
+topic= "velocity"
+port = 1883
+
+# Callbacks
+def on_connect(self, mosq, obj, rc):
+    print("Connected rc: " + str(rc))
+
+def on_message(mosq, obj, msg):
+    print("[Received] Topic: " + msg.topic + ", Message: " + str(msg.payload) + "\n");
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+    print("Subscribed OK")
+
+def on_unsubscribe(mosq, obj, mid, granted_qos):
+    print("Unsubscribed OK")
+
+# Set callbacks
+mqttc.on_message = on_message
+mqttc.on_connect = on_connect
+mqttc.on_subscribe = on_subscribe
+mqttc.on_unsubscribe = on_unsubscribe
+
+# Connect and subscribe
+print("Connecting to " + host + "/" + topic)
+mqttc.connect(host, port=1883, keepalive=60)
+mqttc.subscribe(topic, 0)
+
 # XBee setting
 serdev = '/dev/ttyUSB0'
 s = serial.Serial(serdev, 9600)
@@ -60,6 +93,7 @@ while num != 4:
     s.write("/getAcc/run\r".encode())
     time.sleep(1)
     num += 1
+
 c = s.read(1)
 num = 0
 while num != 200:
@@ -68,7 +102,14 @@ while num != 200:
     number_log.append(float(line))
     print(line)
     num += 1
-number_log[0] = 2
+
+num = 0
+while num != 200:
+    mesg = str(number_log[num])
+    mqttc.publish(topic, mesg)
+    print(mesg)
+    num += 1
+    time.sleep(1)
 
 
 s.close()
